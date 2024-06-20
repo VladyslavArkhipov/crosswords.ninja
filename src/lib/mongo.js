ae1264c85cfa2d7786988002b8bac5b6e35751bc
Vlad <arkhipovvladyslav@gmail.com>
2024-06-20 20:38:00 +0200

    changed back

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,23 +1,13 @@
 import mongoose from "mongoose";
 
 export async function dbConnect() {
-  let conn = null;
   try {
     console.log("Connecting to db");
-    conn = await mongoose.connect(
-      String(process.env.MONGO_DB_CONNECTION_STRING),
-      { useNewUrlParser: true, useUnifiedTopology: true }
+    let conn = await mongoose.connect(
+      String(process.env.MONGO_DB_CONNECTION_STRING)
     );
-    console.log("Connected to db");
     return conn;
   } catch (e) {
-    console.error("Error connecting to db:", e);
     throw new Error(e);
-  } finally {
-    if (conn) {
-      console.log("Closing db connection");
-      await conn.disconnect();
-      console.log("Db connection closed");
-    }
   }
 }
