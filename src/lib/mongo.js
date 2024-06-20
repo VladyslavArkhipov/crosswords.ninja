8dbf13838805d0c54b53190f0ecbbea75c7a33c6
Vlad <arkhipovvladyslav@gmail.com>
2024-06-20 20:02:07 +0200

    i try to fix db

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 
 export async function dbConnect() {
+  let conn = null;
   try {
     console.log("Connecting to db");
-    let conn = await mongoose.connect(
-      String(process.env.MONGO_DB_CONNECTION_STRING)
+    conn = await mongoose.connect(
+      String(process.env.MONGO_DB_CONNECTION_STRING),
+      { useNewUrlParser: true, useUnifiedTopology: true }
     );
+    console.log("Connected to db");
     return conn;
   } catch (e) {
+    console.error("Error connecting to db:", e);
     throw new Error(e);
+  } finally {
+    if (conn) {
+      console.log("Closing db connection");
+      await conn.disconnect();
+      console.log("Db connection closed");
+    }
   }
 }
