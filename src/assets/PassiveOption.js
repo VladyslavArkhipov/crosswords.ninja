52c35b53ec9cefd01140a82c0c591de7a1a6d0f7
Vlad <arkhipovvladyslav@gmail.com>
2024-08-14 15:36:54 +0200

    hot fix

--- src/assets/PassiveOption.js
+++ src/assets/PassiveOption.js
@@ -1,8 +1,8 @@
 export default function PassiveOption() {
   return (
     <svg
-      width="26"
-      height="26"
+      width="24"
+      height="24"
       viewBox="0 0 26 26"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
