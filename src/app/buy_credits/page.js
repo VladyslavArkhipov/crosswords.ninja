6abd22322c06a2fe0d911a94df35f7408ae99a64
Vlad <arkhipovvladyslav@gmail.com>
2024-11-11 19:38:45 +0100

    try to make it

--- src/app/buy_credits/page.js
+++ src/app/buy_credits/page.js
@@ -2,18 +2,13 @@ import BuyCreditsContent from "@/components/buyCredits/BuyCreditsContent";
 import { auth } from "@/utils/auth";
 import { User } from "@/model/user-model";
 
-// Создаём кеш объекта
-let userCache = null;
-
 export default async function BuyCredits() {
   const session = await auth();
+  const user = session?.user;
+  console.log("[BuyCredits] Loaded user:", user);
 
-  // Если пользователь уже закеширован, используем его
-  if (userCache && userCache.email === session?.user?.email) {
-    console.log(
-      "[BuyCredits] Используем кешированного пользователя:",
-      userCache
-    );
-    return <BuyCreditsContent user={userCache} />;
+  // Проверка наличия пользователя перед рендерингом контента
+  if (user) {
+    return <BuyCreditsContent user={user} />; // или соответствующий компонент загрузки
   }
 }
