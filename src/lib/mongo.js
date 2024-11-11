889d5cae26749c47db3c1e28aed0e2428745aee6
Vlad <arkhipovvladyslav@gmail.com>
2024-11-11 20:21:32 +0100

    try

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 export async function dbConnect() {
   try {
     console.log("Connecting to db");
-    let conn = await mongoose.connect(
-      String(process.env.MONGO_DB_CONNECTION_STRING)
-    );
+    let conn = await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     return conn;
   } catch (e) {
     throw new Error(e);
